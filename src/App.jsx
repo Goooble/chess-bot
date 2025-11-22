b659c5e2620c6bdf70aae36fe255c267fe699502
Goooble <bivinbheemaiah@gmail.com>
2025-11-22 17:03:46 +0530

    feat: flip and reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { ChessGame } from "@react-chess-tools/react-chess-game";
 import "./App.css";
+import Menu from "./Menu";
 
 function App() {
   return (
-    <div className="flex justify-center">
+    <div className="flex items-center flex-col gap-5">
       <ChessGame.Root>
-        <div className="w-3/5">
+        <div className="w-2/5">
           <ChessGame.Board />
         </div>
+        <Menu></Menu>
       </ChessGame.Root>
     </div>
   );
