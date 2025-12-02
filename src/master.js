d3ca803685dc755a63dc3acedb9375f57d7b0e2f
Goooble <bivinbheemaiah@gmail.com>
2025-12-03 00:41:48 +0530

    feat: randomizer(might be buggy)

--- src/master.js
+++ src/master.js
@@ -15,16 +15,17 @@ export function botMove(engine) {
   // let score = evaluate(board) / 100;
   // console.log(score);
   // const moves = chess.moves({ verbose: true });
-  const searchEngine = Chess(engine.currentFen);
+  const searchEngine = new Chess(engine.currentFen);
   const bestMove = search(searchEngine);
   return bestMove;
   // return moves[Math.floor(Math.random() * moves.length)];
 }
 
 //search
-
+let prevMaxScore = 0; //to generate random moves
 function search(chess) {
   const moves = chess.moves({ verbose: true });
+  const randomMove = [moves[0]]; //to generate random moves when no difference in scores
   let bestMove = moves[0];
   let maxScore = -Infinity;
   for (let element of moves) {
@@ -34,13 +35,21 @@ function search(chess) {
       promotion: element.promotion,
     });
     let score = -1 * negaMax(chess, 2);
+    if (score === maxScore) {
+      randomMove.push(element);
+    }
     if (score > maxScore) {
       maxScore = score;
       bestMove = element;
     }
+
     chess.undo();
   }
+  // console.log("Prev Score " + prevMaxScore);
   console.log("Best Score: " + maxScore);
+  if (prevMaxScore === maxScore)
+    return randomMove[Math.floor(Math.random() * randomMove.length)];
+  prevMaxScore = maxScore;
   return bestMove;
 }
 
